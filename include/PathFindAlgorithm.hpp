ac1b6fb81dff9e3eab0c80f607622a0f0f0b576e
htsai51 <htsai51@terpmail.umd.edu>
2017-03-11 13:33:49 -0500

    Task 3.1 Separate Classes into Files
    Separate Node and Edge classes from PathFindingAlgorithm class
    into individual files

--- include/PathFindAlgorithm.hpp
+++ include/PathFindAlgorithm.hpp
@@ -1,14 +1,33 @@
 /********************************************************************
- *                                                                  *
- * Copyright (C) 2017 by Huei Tzu Tsai                              *
- *                                                                  *
+MIT License
+
+Copyright (c) 2017 Huei-Tzu Tsai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  ********************************************************************/
 
 /** @file PathFindAlgorithm.hpp
  *  @brief Definition of class PathFindAlgorithm
  *
- *  This file contains definitions of class pathFindAlgorithm and
- *  its component classes node and edge
+ *  This file contains definitions of class pathFindAlgorithm which
+ *  implements the basic functions needed for path planning to
+ *  compute the shortest path.
  *
  *  @author Huei Tzu Tsai
  *  @date   03/07/2017
@@ -17,10 +36,9 @@
 #ifndef INCLUDE_PATHFINDALGORITHM_HPP_
 #define INCLUDE_PATHFINDALGORITHM_HPP_
 
-#include <string>
 #include <vector>
-#include <limits>
-#include <tuple>
+#include "Node.hpp"
+#include "Edge.hpp"
 #include "Map.hpp"
 
 
@@ -29,68 +47,8 @@
 
 
 /**
- *  @brief Class that maintains map information of a node 
-*/
-class Node {
- public:
-     Node(int i, double x, double y)                         ///< constructor
-         : index(i), parentIndex(0), xPos(x), yPos(y) {
-         // initialize cost, estimateCost to MAX integer
-         cost = std::numeric_limits<int>::max();
-         estimateCost = std::numeric_limits<int>::max();
-     }
-     ~Node() {}                                              ///< deconstructor
-
-     int getIndex(void) { return index; }
-
-     void setParentIndex(int index) { parentIndex = index; }
-     double getParentIndex(void) { return parentIndex; }
-
-     void setCost(double newCost) { cost = newCost; }
-     double getCost(void) { return cost; }
-
-     void setEstimateCost(double newEstCost) { estimateCost = newEstCost; }
-     double getEstimateCost(void) { return estimateCost; }
-
-     std::tuple<double, double> getPos(void)
-         { return std::make_tuple(xPos, yPos); }
-
- private:
-     int index;                                    ///< index
-     int parentIndex;                              ///< parentIndex
-
-     double cost;                                  ///< cost from start to node
-     double estimateCost;                          ///< heuristic cost to goal
-
-     double xPos;                                  ///< x position
-     double yPos;                                  ///< y position
-};
-
-
-/**
- *  @brief Class that maintains map information of edge cost
- *         between start and end nodes
-*/
-class Edge {
- public:
-     Edge(int s, int e, double c)                            ///< constructor
-         : startIndex(s), endIndex(e), cost(c) {}
-     ~Edge() {}
-
-     int getStartIndex(void) { return startIndex; }
-     int getEndIndex(void) { return endIndex; }
-     double getCost(void) { return cost; }
-
- private:
-     int startIndex;                                 ///< start node index
-     int endIndex;                                   ///< end node index
-     double cost;                                    ///< edge cost
-};
-
-
-/**
- *  @brief Class that implements base class of PathFinding
- *         algorithm.
+ *  @brief Class that implements the basic functions
+ *         reqiured for PathFinding algorithm.
 */
 class PathFindingAlgorithm {
  public:
@@ -112,7 +70,7 @@ class PathFindingAlgorithm {
 
      void OutputPath(int);                  ///< output path on screen/file
 
-     std::vector<int>& getPath()            ///< get path vectors
+     std::vector<int> getPath()            ///< get path vectors
                            { return path; }
 
      double getTotalCost()
